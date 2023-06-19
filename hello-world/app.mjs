6682d4f1a814f035247933522669ca02d085ce92
Calvin C Chan <hello@calvin-c.com>
2023-06-19 14:51:00 -0700

    chore: testing sharp

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -3,24 +3,50 @@
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 
-export const lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
+import sharp from 'sharp';
+
+async function generateSquareImage() {
+  // Set the dimensions of the image
+  const width = 400;
+  const height = 400;
+
+  // Create a new Sharp image with a red square
+  const image = await sharp({
+    create: {
+      width: width,
+      height: height,
+      channels: 3, // 3 channels: Red, Green, Blue
+      background: { r: 255, g: 0, b: 0 } // Red color
     }
+  }).jpeg().toBuffer();
+
+  return image;
+}
+
+
+
+export const lambdaHandler = async (event, context) => {
+  try {
+    const buffer = await generateSquareImage();
+    return {
+      'statusCode': 200,
+      'headers': {
+        'Content-Type': 'image/jpeg',
+        'Content-Length': buffer.length.toString()
+      },
+      'body': buffer.toString('base64'),
+      isBase64Encoded: true
+    }
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
