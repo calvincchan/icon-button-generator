b4b68de72f46b15a6cffb3d9d451e5db7d879e0b
Calvin C Chan <hello@calvin-c.com>
2023-06-24 06:33:37 -0700

    chore: mute aws deploy message

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 FUNC_ARN=arn:aws:lambda:ap-southeast-1:917253244281:function:GenerateIconButton
 ZIP_FILE=deploy.zip
 zip $ZIP_FILE ./index.mjs
-aws lambda update-function-code --function-name $FUNC_ARN --zip-file fileb://$ZIP_FILE
+aws lambda update-function-code --function-name $FUNC_ARN --zip-file fileb://$ZIP_FILE > /dev/null
 rm $ZIP_FILE
