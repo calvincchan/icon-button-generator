ef1016115e3e414ced242d00d2d25abd28726272
Calvin C Chan <hello@calvin-c.com>
2023-06-24 13:01:34 -0700

    chore: improve batch building

--- build.js
+++ build.js
@@ -1,14 +1,36 @@
 const fs = require("fs");
-const ICON_PACKAGE = process.argv[process.argv.length - 1];
-const json = require(`@svg-icons/${ICON_PACKAGE}/__manifest.json`);
+const buildPackages = require("./build.config.json");
 
-const icons = json.map((icon) => {
-  const { name, ...rest } = icon;
-  const svg = fs.readFileSync(
-    `./node_modules/@svg-icons/${ICON_PACKAGE}/${name}.svg`,
-    { encoding: "utf8", flag: "r" }
-  );
-  return [name, { svg }];
-});
+function generateIndexFile(buildPackages) {
+  return `export const fontPackages = new Set(${JSON.stringify(
+    buildPackages
+  )});`;
+}
 
-console.log(`export const IconMap = new Map(${JSON.stringify(icons)});`);
+function generateSvgFile(buildPackage) {
+  const json = require(`@svg-icons/${buildPackage}/__manifest.json`);
+  const icons = json.map((icon) => {
+    const { name, ...rest } = icon;
+    const svg = fs.readFileSync(
+      `./node_modules/@svg-icons/${buildPackage}/${name}.svg`,
+      { encoding: "utf8", flag: "r" }
+    );
+    return [name, { svg }];
+  });
+  return `export const IconMap = new Map(${JSON.stringify(icons)});`;
+}
+
+function main() {
+  /** index file */
+  fs.writeFileSync("./assets/index.mjs", generateIndexFile(buildPackages));
+
+  /** svg files */
+  for (let iconPackage of buildPackages) {
+    fs.writeFileSync(
+      `./assets/${iconPackage}.mjs`,
+      generateSvgFile(iconPackage)
+    );
+  }
+}
+
+main();
